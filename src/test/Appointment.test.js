5c5fd6599bf88a6a6e99d5c757b96d54b1d682c6
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-04-07 20:42:15 -0500

    first test to Appointmet

--- src/test/Appointment.test.js
+++ src/test/Appointment.test.js
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
-iport { Appoinment } from '../src/Appointment'
+import { Appointment } from '../Appointment';
 
 describe('Appointment', () => {
 	it('renders the customer first name', () => {
 		const customer = { firstName: 'Bernardo' };
-		render(<Appointment customer={customer} />);
-		expect(document.body.textContent).toMatch('Bernardo');
+		const { getByText } = render(<Appointment customer={customer} />);
+		expect(getByText('Bernardo')).toBeTruthy()
 	});
 });
